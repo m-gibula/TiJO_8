7c04fbb605cc923472c4067dba07c2822540f66b
m-gibula <gibula27135@student.pwsztar.edu.pl>
2017-05-02 17:24:16 +0200

    part1 - raport1

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,21 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('sth', function(){
+        expect(app.reverseNumber(1234)).to.eql(4321);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('sth', function(){
+        expect(app.returnOnlyLetter("AF98af987")).to.eql("af");
+            });
 });
 
 describe('isEmail', function ()
 {
-
+    it('sth', function(){
+        expect(app.isEmail("asdffa234.tk")).to.eql(false);
+           });
 });
