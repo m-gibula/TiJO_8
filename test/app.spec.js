a8bf1e54d62ab997e0b27a3b4337f811d4655b8b
m-gibula <gibula27135@student.pwsztar.edu.pl>
2017-05-02 17:36:59 +0200

    part2 - report2 - 95.45%

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,6 +8,11 @@ describe('reverseNumber', function ()
     it('sth', function(){
         expect(app.reverseNumber(1234)).to.eql(4321);
     });
+
+    it('if(isNaN(num))', function(){
+        expect(app.reverseNumber("imNaN")).to.eql(false);
+    });
+
 });
 
 describe('returnOnlyLetter', function ()
@@ -15,6 +20,10 @@ describe('returnOnlyLetter', function ()
     it('sth', function(){
         expect(app.returnOnlyLetter("AF98af987")).to.eql("af");
             });
+    it('you shall not pass!', function(){
+        expect(app.returnOnlyLetter(997)).to.eql(false);
+    });
+
 });
 
 describe('isEmail', function ()
@@ -22,4 +31,7 @@ describe('isEmail', function ()
     it('sth', function(){
         expect(app.isEmail("asdffa234.tk")).to.eql(false);
            });
+    it('sth2', function(){
+        expect(app.isEmail("asdffa@234.tk")).to.eql(true);
+    });
 });
